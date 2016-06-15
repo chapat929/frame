a073d5fb610151b09509e572c58d07eef6dbd47a
Chandni Patel <chandni_patel@gap.com>
2016-06-15 08:11:19 -0700

    Have button perform action when clicked

---
+++ src/main/java/com/chandni/Main.java
@@ -0,0 +1,8 @@
+package com.chandni;
+
+public class Main {
+    public static void main(String[] args) {
+        final SimpleGui1 simpleGui1 = new SimpleGui1();
+        simpleGui1.go();
+    }
+}
--- src/main/java/com/chandni/SimpleGui1.java
+++ src/main/java/com/chandni/SimpleGui1.java
@@ -1,16 +1,28 @@
 package com.chandni;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SimpleGui1 {
-    public static void main(String[] args) {
+public class SimpleGui1 implements ActionListener {
+
+    private JButton button;
+
+    public void go() {
         final JFrame frame = new JFrame();
-        final JButton button = new JButton("click me");
+        button = new JButton("click me");
+        button.addActionListener(this);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.getContentPane().add(button);
         frame.setSize(300, 300);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        button.setText("I've been clicked");
+
     }
 }
